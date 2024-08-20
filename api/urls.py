dff62d949c26f3943c4a29c884755bb0bf97d04e
Muhammadrasul2711 <146407951+Muhammadrasul2711@users.noreply.github.com>
2024-08-20 18:44:58 +0500

    afasdf

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('categories/<int:id>/', CategoryDetail.as_view(), name='category-detail'),
     path('products/', ProductList.as_view(), name='product-list'),
     path('products/<int:id>/', ProductDetail.as_view(), name='product-detail'),
+    path('log-in/',views.login),
 ]
--- api/views.py
+++ api/views.py
@@ -8,6 +8,8 @@ from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate, login
 from .Serializer import CategorySerializer,ProductSerializer
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -46,3 +48,52 @@ def login(request):
         }
     return Response(context)
 
+
+@api_view(['POST'])
+def register(request):
+    serializer = serializers.UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+@api_view(['GET'])
+def logout(request):
+    request.user.auth_token.delete()
+    return Response(status=204)
+
+# =================================================================
+
+
+
+class ProductCreateView(APIView):
+    def post(self, request):
+        serializer = serializers.ProductDetailSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE'])
+def product_delete(request, pk):
+    product = get_object_or_404(models.Product, pk=pk)
+    product.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class AddToCartView(APIView):
+    @login_required
+    def post(self, request, product_id):
+        product = get_object_or_404(models.Product, id=product_id)
+        cart, _ = models.Cart.objects.get_or_create(author=request.user, is_active=True)
+        product_img = product.images.first()
+        cart_product, _ = models.CartProduct.objects.get_or_create(
+            productImg=product_img,
+            product=product,
+            cart=cart
+        )
+        cart_product.quantity += 1
+        cart_product.total_price = cart_product.quantity * product.price
+        cart_product.save()
+        return Response(status=status.HTTP_200_OK)
